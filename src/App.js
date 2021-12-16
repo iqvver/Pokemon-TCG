651e6f38d488b59ab64674e13099931a88f8c257
iqvver <91122487+iqvver@users.noreply.github.com>
2021-12-16 18:33:17 +0200

    README

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import * as React from 'react';
-import Header from './Componetns/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import Login from './Componetns/Login/Login';
+import { Route } from 'react-router-dom';
 import PokeCardContainer from './Componetns/Content/Card/PokeCardContainer';
 import CardContainer from './Componetns/Content/Card/CardContainer';
 import LoginContainer from './Componetns/Login/LoginContainer';
@@ -15,15 +13,12 @@ function App() {
         <HeaderContainer />
       </header>
       <div className='App-navBar'>
-
       </div>
       <div className='App-content'>
         <div>
-          
-            <Route exact path='/' component={ PokeCardContainer } />
-            <Route exact path='/login' component={ LoginContainer } />
-            <Route path='/cards/:pokeId?' render={() => <CardContainer /> }/>
-          
+          <Route exact path='/' component={PokeCardContainer} />
+          <Route exact path='/login' component={LoginContainer} />
+          <Route path='/cards/:pokeId?' render={() => <CardContainer />} />
         </div>
       </div>
     </div>
