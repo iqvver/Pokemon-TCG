2666465d415befe1754a47597827c4641f97d57b
iqvver <91122487+iqvver@users.noreply.github.com>
2021-12-19 13:24:38 +0200

    hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
@@ -10,11 +10,13 @@ import '@fontsource/roboto/500.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    </BrowserRouter>
+    <HashRouter>
+      
+        <Provider store={store}>
+          <App />
+        </Provider>
+      
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
